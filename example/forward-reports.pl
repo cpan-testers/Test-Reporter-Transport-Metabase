b72994f57af08077c3ef3588c84607d6fd558f60
David Golden <dagolden@cpan.org>
2009-05-11 00:03:07 -0400

    changed example to use test profile file

--- example/forward-reports.pl
+++ example/forward-reports.pl
@@ -6,6 +6,8 @@ use lib "$FindBin::Bin/../lib";
 use Path::Class;
 use Test::Reporter;
 
+my $profile = file($0)->dir->file('profile.json');
+print "Using profile in '$profile'\n";
 my $report_dir = file($0)->dir->subdir('reports');
 print "Looking for reports in '$report_dir'\n";
 
@@ -14,7 +16,10 @@ for my $f ( $report_dir->children ) {
   print "$f\n";
   my $tr = Test::Reporter->new(
     transport => 'Metabase',
-    transport_args => [ 'http://localhost:3000/', '123456', 'abcdef' ]
+    transport_args => [ 
+      uri => 'http://localhost:3000/',
+      profile => $profile,
+    ],
   );
   $tr->read( $f );
   # XXX complete hack until backpan index mapping is done
