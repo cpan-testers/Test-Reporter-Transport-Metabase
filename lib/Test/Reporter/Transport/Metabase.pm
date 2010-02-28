1a69d91833a08837acd584fa49076d1e5d48b2a5
David Golden <dagolden@cpan.org>
2010-02-28 18:12:03 -0500

    bump version to 0.002

--- lib/Test/Reporter/Transport/Metabase.pm
+++ lib/Test/Reporter/Transport/Metabase.pm
@@ -2,7 +2,7 @@ package Test::Reporter::Transport::Metabase;
 use 5.006;
 use warnings;
 use strict;
-our $VERSION = 0.001;
+our $VERSION = '0.002';
 use base 'Test::Reporter::Transport';
 
 use Carp                      ();
